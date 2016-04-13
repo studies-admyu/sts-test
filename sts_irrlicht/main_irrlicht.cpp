400d4b58a83cbeb796025d7500f6360dcbde8b5f
studies-admyu <studies-admyu@users.noreply.github.com>
2016-04-14 02:15:18 +0300

    Added model & animated sprite examples for Irrlicht and OGRE.

--- sts_irrlicht/main_irrlicht.cpp
+++ sts_irrlicht/main_irrlicht.cpp
@@ -1,5 +1,6 @@
 #include <irrlicht.h>
 #include <driverChoice.h>
+#include <cmath>
 
 #include <iostream>
 
@@ -12,15 +13,55 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	device->setWindowCaption(L"STS Test");
+	device->setWindowCaption(L"CTS Test");
 	irr::video::IVideoDriver* driver = device->getVideoDriver();
 	irr::scene::ISceneManager* smgr = device->getSceneManager();
 	irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-	smgr->addCameraSceneNode(0, irr::core::vector3df(0, 30, -40), irr::core::vector3df(0, 5, 0));
+	irr::scene::IAnimatedMesh* mesh = smgr->getMesh("media/airship.x");
+	if (!mesh) {
+		std::cerr << "Mesh is not loaded." << std::endl;
+		device->drop();
+		return 1;
+	}
+
+	irr::core::vector3df shipSP(0, -200, -100);
+	irr::scene::IMeshSceneNode* node = smgr->addMeshSceneNode(mesh);
+	if (node) {
+		node->setScale(irr::core::vector3df(6.3, 6.3, 6.3));
+		node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+		node->setMaterialTexture(0, driver->getTexture("media/all_diffuse.jpg"));
+		node->setPosition(shipSP);
+	}
+
+	smgr->addCameraSceneNode(0, irr::core::vector3df(0, 100, -1), irr::core::vector3df(0, 0, 0));
+
+	irr::video::ITexture* images = driver->getTexture("media/ship1.png");
+
+	irr::scene::IBillboardSceneNode* billboard = smgr->addBillboardSceneNode(nullptr, irr::core::dimension2d<irr::f32>(48.0f, 58.0f));
+	if (billboard) {
+		billboard->setMaterialTexture(0, images);
+		billboard->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+		billboard->setMaterialType(irr::video::EMT_TRANSPARENT_ALPHA_CHANNEL);
+		billboard->setPosition(irr::core::vector3df(0, -200, 100));
+
+		billboard->getMaterial(0).getTextureMatrix(0).setTextureScale(0.25, 1.0);
+	}
+
+	irr::ITimer* timer = device->getTimer();
 
 	while (device->run()) {
-		driver->beginScene(true, true, irr::video::SColor(255, 100, 101, 140));
+		driver->beginScene(true, true, irr::video::SColor(255, 255, 0, 255));
+
+		irr::u32 currentTime = timer->getTime();
+		float angle = sin(float(currentTime) * irr::core::PI / 2000.0f) * 45.0f;
+		float diplacement = cos(float(currentTime) * irr::core::PI / 2000.0f) * 100.0f;
+		node->setRotation(irr::core::vector3df(0.0f, 0.0f, angle));
+		node->setPosition(shipSP + irr::core::vector3df(diplacement, 0.0f, 0.0f));
+
+		unsigned int currentFrame = (currentTime / 125) % 2;
+
+		billboard->getMaterial(0).getTextureMatrix(0).setTextureTranslate(0.25 * currentFrame, 0.0);
 
 		smgr->drawAll();
 		guienv->drawAll();
--- sts_ogre/main_ogre.cpp
+++ sts_ogre/main_ogre.cpp
@@ -7,6 +7,9 @@
 #include <OGRE/OgreRenderSystem.h>
 #include <OGRE/OgreRenderWindow.h>
 #include <OGRE/OgreWindowEventUtilities.h>
+#include <OGRE/OgreEntity.h>
+#include <OGRE/OgreBillboardSet.h>
+#include <OGRE/OgreBillboard.h>
 
 typedef std::vector<Ogre::String> Strings;
 
@@ -42,6 +45,15 @@ void initRenderSystem(std::unique_ptr<Ogre::Root>& root)
 	root->setRenderSystem(lRenderSystem);
 }
 
+void initResourceGroup(const Ogre::String& groupName, const Ogre::String& fsPath)
+{
+	Ogre::ResourceGroupManager& lRcGroupMgr = Ogre::ResourceGroupManager::getSingleton();
+	lRcGroupMgr.createResourceGroup(groupName);
+	lRcGroupMgr.addResourceLocation(fsPath, "FileSystem", groupName, false);
+	lRcGroupMgr.initialiseResourceGroup(groupName);
+	lRcGroupMgr.loadResourceGroup(groupName);
+}
+
 int main(int argc, char* argv[])
 {
 	try {
@@ -71,7 +83,80 @@ int main(int argc, char* argv[])
 			lWindow = lRoot->createRenderWindow(lWindowTitle, lSizeX, lSizeY, lFullscreen, &lParams);
 		}
 
+		/* Create a scene manager */
+		Ogre::SceneManager* lScene = lRoot->createSceneManager(Ogre::ST_GENERIC, "SceneManager");
+
+		Ogre::SceneNode* lRootSceneNode = lScene->getRootSceneNode();
+
+		/* Create camera */
+		Ogre::Camera* lCamera = lScene->createCamera("MyCamera");
+
+		/* Create viewport (camera <-> window) */
+		Ogre::Viewport* vp = lWindow->addViewport(lCamera);
+
+		vp->setAutoUpdated(true);
+		vp->setBackgroundColour(Ogre::ColourValue(1, 0, 1));
+
+		lCamera->setAspectRatio(float(vp->getActualWidth()) / vp->getActualHeight());
+		lCamera->setPosition(Ogre::Vector3(0, 100, -1));
+		lCamera->lookAt(Ogre::Vector3(0, 0, 0));
+
+		/* Set clipping*/
+		lCamera->setNearClipDistance(1.5f);
+		lCamera->setFarClipDistance(3000.0f);
+
+		/* Lighting */
+		Ogre::Light* lLight = lScene->createLight("MainLight");
+		lLight->setPosition(Ogre::Vector3(0, 100, 0));
+
+		/* Resource manager */
+		Ogre::String lRcGroupName = "Test group";
+		Ogre::String lRcGroupPath = "media";
+		initResourceGroup(lRcGroupName, lRcGroupPath);
+
+		/* Load model */
+		Ogre::Entity* lShipEntity = lScene->createEntity("airship.mesh");
+		lShipEntity->setCastShadows(false);
+
+		Ogre::SceneNode* lShipNode = lRootSceneNode->createChildSceneNode();
+		lShipNode->attachObject(lShipEntity);
+		lShipNode->setScale(Ogre::Vector3(3.15f, 3.15f, 3.15f));
+
+		/* Starship start point */
+		Ogre::Vector3 razorSP(0, -200, -100);
+		lShipNode->setPosition(razorSP);
+
+		/* Sprite billboard */
+		Ogre::SceneNode* lSpriteNode = lRootSceneNode->createChildSceneNode();
+		Ogre::BillboardSet* lBillboardSet = lScene->createBillboardSet();
+		lBillboardSet->setMaterialName("ship1", lRcGroupName);
+		lBillboardSet->setTextureStacksAndSlices(1, 4);
+		Ogre::Billboard* lSpriteBillboard = lBillboardSet->createBillboard(Ogre::Vector3(0, 0, 0));
+		lSpriteBillboard->setDimensions(48.0f / 2.0f, 58.0f / 2.0f);
+		lSpriteBillboard->setTexcoordIndex(1);
+		lSpriteNode->attachObject(lBillboardSet);
+		lSpriteNode->setPosition(Ogre::Vector3(0, -200, 100));
+
+		/* Obtain the timer pointer */
+		Ogre::Timer* lTimer = lRoot->getTimer();
+
+		/* Skip all the messages */
+		lWindow->setAutoUpdated(false);
+		lRoot->clearEventTimes();
+
 		while (!lWindow->isClosed()) {
+			float angle = Ogre::Math::Sin(float(lTimer->getMilliseconds()) * Ogre::Math::PI / 2000.0f) * Ogre::Math::PI / 4.0f;
+			float diplacement = Ogre::Math::Cos(float(lTimer->getMilliseconds()) * Ogre::Math::PI / 2000.0f) * 100.0f;
+			lShipNode->setOrientation(Ogre::Quaternion(Ogre::Radian(angle), Ogre::Vector3(0, 0, 1)));
+			lShipNode->setPosition(razorSP + Ogre::Vector3(diplacement, 0.0f, 0.0f));
+
+			unsigned int spriteFrame = (lTimer->getMilliseconds() / 125) % 2;
+			lSpriteBillboard->setTexcoordIndex(spriteFrame);
+
+			lWindow->update(false);
+			lWindow->swapBuffers();
+			lRoot->renderOneFrame();
+
 			Ogre::WindowEventUtilities::messagePump();
 		}
 		Ogre::LogManager::getSingleton().logMessage("Render window closed.");
